a8d2dceefbcc36557d7fe5d8a60d975c43a8fd4f
张子山 <995645888@qq.com>
2020-11-03 15:09:34 +0800

    [D] 删除关系映射

--- src/Support/Middleware/ValidateMiddleware.php
+++ src/Support/Middleware/ValidateMiddleware.php
@@ -2,21 +2,20 @@
 
 namespace W7\Validate\Support\Middleware;
 
+use Exception;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use W7\Core\Facades\Container;
 use W7\Core\Middleware\MiddlewareAbstract;
-use W7\Validate\Support\Storage\ValidateRelation;
+use W7\Validate\Support\Storage\ValidateConfig;
+use W7\Validate\Validate;
 
 class ValidateMiddleware extends MiddlewareAbstract
 {
 	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
 	{
-		$scene = null;
-		/** @var ValidateRelation $validateRelation */
-		$validateRelation = Container::get(ValidateRelation::class);
-		$validator        = $validateRelation->getValidate($request);
+		$scene     = null;
+		$validator = $this->getValidate($request);
 		if (false === $validator) {
 			$data = [];
 		} else {
@@ -26,5 +25,29 @@ class ValidateMiddleware extends MiddlewareAbstract
 		}
 		$request = $request->withAttribute('validate', $data);
 		return $handler->handle($request);
+	}
+	
+	final public function getValidate(ServerRequestInterface $request)
+	{
+		$route      = $request->getAttribute('route');
+		$controller = $route['controller'] ?? '';
+		$scene      = $route['method']     ?? '';
+		
+		$validate   = str_replace(ValidateConfig::instance()->controllerPath, '', $controller);
+		$_namespace = explode('\\', $validate);
+		$fileName   = str_replace('Controller', 'Validate', array_pop($_namespace));
+		$validate   = ValidateConfig::instance()->validatePath . implode('\\', $_namespace) . '\\' . $fileName;
+		
+		if (class_exists($validate)) {
+			if (is_subclass_of($validate, Validate::class)) {
+				/** @var Validate $validator */
+				$validator = new $validate($request);
+				$validator->scene($scene);
+				return $validator;
+			}
+			
+			throw new Exception("The given 'Validate' " . $validate . ' has to be a subtype of W7\Validate\Validate');
+		}
+		return false;
 	}
 }
