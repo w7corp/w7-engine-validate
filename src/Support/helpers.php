6907caf3946bda33ba7065837286e2851405729c
虞灪 <995645888@qq.com>
2020-11-04 10:25:24 +0800

    [A] 增加从上下文获取验证值

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,23 +1,32 @@
 <?php
 
+use Psr\Http\Message\ServerRequestInterface;
+use W7\Core\Facades\Context;
+use W7\Validate\Support\Storage\ValidateCollection;
+
 if (!function_exists('validate_collect')) {
 	/**
+	 * 数值转为验证器集合ValidateCollection类型
 	 * @param null $value
-	 * @return \W7\Validate\Support\Storage\ValidateCollection
+	 * @return ValidateCollection
 	 */
 	function validate_collect($value = null)
 	{
-		return new \W7\Validate\Support\Storage\ValidateCollection($value);
+		return new ValidateCollection($value);
 	}
 }
 
 if (!function_exists('get_validate_data')) {
 	/**
-	 * @param \Psr\Http\Message\ServerRequestInterface $request
-	 * @return \W7\Validate\Support\Storage\ValidateCollection
+	 * 获取验证后的结果
+	 * @param ServerRequestInterface|null $request 请求示例，如果为null，则自动从上下文中获取
+	 * @return ValidateCollection 返回验证器集合ValidateCollection类型
 	 */
-	function get_validate_data(\Psr\Http\Message\ServerRequestInterface $request)
+	function get_validate_data(ServerRequestInterface $request = null)
 	{
+		if (null === $request) {
+			$request = Context::getRequest();
+		}
 		return validate_collect($request->getAttribute('validate'));
 	}
 }
