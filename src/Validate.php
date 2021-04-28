af190cb6d242831acf4a80ac61024596de0fa951
虞灪 <995645888@qq.com>
2021-04-28 18:04:50 +0800

    [U] 优化事件结果处理代码

--- src/Validate.php
+++ src/Validate.php
@@ -283,16 +283,7 @@ class Validate
 
         $data = $pipeline($data);
 
-        if (is_string($data)) {
-            if (isset($this->message[$data])) {
-                $data = $this->replacingFieldsInMessage($this->message[$data]);
-            }
-            throw new ValidateException($data, 403);
-        } elseif (is_array($data)) {
-            return $data;
-        }
-
-        throw new LogicException('Validate event return type error');
+        return $this->handleEventResult($data);
     }
 
     /**
@@ -308,6 +299,17 @@ class Validate
             return $data;
         }
         $result = (new ValidateHandler($data, $this->handlers, $this->currentScene))->handle($method);
+        return $this->handleEventResult($result);
+    }
+
+    /**
+     * 处理事件结果
+     * @param $result
+     * @return array
+     * @throws ValidateException
+     */
+    private function handleEventResult($result): array
+    {
         if (is_string($result)) {
             if (isset($this->message[$result])) {
                 $result = $this->replacingFieldsInMessage($this->message[$result]);
