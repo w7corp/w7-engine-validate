114b1ea46dff83b6b63207791ee096c250899b4c
虞灪 <995645888@qq.com>
2021-09-09 19:06:19 +0800

    [A] 增加SomeTimes规则的单元测试

--- tests/Test/TestValidateScene.php
+++ tests/Test/TestValidateScene.php
@@ -15,8 +15,34 @@ namespace W7\Tests\Test;
 use W7\Tests\Material\ArticleValidate;
 use W7\Tests\Material\BaseTestValidate;
 use W7\Validate\Exception\ValidateException;
+use W7\Validate\Support\ValidateScene;
 use W7\Validate\Validate;
 
+class TestSomeTimes extends Validate
+{
+    protected $message = [
+        'a.required' => 'a不能为空',
+        'b.required' => 'b不能为空',
+    ];
+    protected function sceneTest(ValidateScene $scene)
+    {
+        $scene->appendCheckField('a')
+            ->appendCheckField('aIsRequired')
+            ->append('aIsRequired', 'required')
+            ->sometimes('a', 'required', function ($data) {
+                return 1 == $data['aIsRequired'];
+            });
+    }
+
+    protected function sceneTestSomeTimesMultiField(ValidateScene $scene)
+    {
+        $scene->appendCheckField('b')
+            ->appendCheckField('a')
+            ->sometimes(['a', 'b'], 'required', function () {
+                return true;
+            });
+    }
+}
 class TestValidateScene extends BaseTestValidate
 {
     protected $userInput;
@@ -102,6 +128,40 @@ class TestValidateScene extends BaseTestValidate
 
         try {
             $v->scene('notFount')->check([]);
+        } catch (ValidateException $e) {
+            $this->assertCount(2, $e->getData());
+        }
+    }
+
+    /**
+     * @test 测试sometimes规则
+     *
+     * @throws ValidateException
+     */
+    public function testSometimesRule()
+    {
+        $v    = TestSomeTimes::make();
+        $data = $v->scene('test')->check([
+            'aIsRequired' => 0
+        ]);
+
+        $this->assertSame(0, $data['aIsRequired']);
+
+        $this->expectException(ValidateException::class);
+        $this->expectExceptionMessage('a不能为空');
+        $v->scene('test')->check([
+            'aIsRequired' => 1
+        ]);
+    }
+
+    /**
+     * @test 测试sometimes为多个字段附加规则
+     */
+    public function testSometimesMultiField()
+    {
+        $v = TestSomeTimes::make();
+        try {
+            $v->scene('testSomeTimesMultiField')->check([]);
         } catch (ValidateException $e) {
             $this->assertCount(2, $e->getData());
         }
